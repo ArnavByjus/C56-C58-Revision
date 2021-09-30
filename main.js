34f0fe2a047ef21cf64b01c6401edcbcb8ea6623
ArnavByjus <91581307+ArnavByjus@users.noreply.github.com>
2021-09-29 20:01:46 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function fn_click()
+        {
+            document.getElementById("idh1").innerHTML="bob";
+            document.getElementById("idCat").src="https://i.ytimg.com/vi/QH2-TGUlwu4/sddefault.jpg";
+            window.alert("You clicked the button");
+        }
